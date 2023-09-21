8645c5cbf46c62bd3030f5bbb46a966d312f6db0
kozgunov <78970376+kozgunov@users.noreply.github.com>
2023-09-21 23:42:35 +0300

    Add files via upload

---
+++ signing.js
@@ -0,0 +1,9 @@
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Sign Up Form Submitted');
+});
+
+document.getElementById('signinForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Sign In Form Submitted');
+});
