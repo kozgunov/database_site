118332ebe09413a6d37f418855bb8c05fa96ed78
kozgunov <78970376+kozgunov@users.noreply.github.com>
2023-09-22 15:22:54 +0300

    Update signing.js

--- signing.js
+++ signing.js
@@ -7,3 +7,36 @@ document.getElementById('signinForm').addEventListener('submit', function(event)
     event.preventDefault();
     alert('Sign In Form Submitted');
 });
+
+
+
+function togglePasswordVisibility(passwordFieldId, iconElement) {
+    const passwordField = document.getElementById(passwordFieldId);
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        iconElement.src = "eye-slash-icon.png"; // Path to the 'eye-slash' icon
+    } else {
+        passwordField.type = "password";
+        iconElement.src = "eye-icon.png"; // Path to the 'eye' icon
+    }
+}
+
+function openAssistant() {
+    // Implement the functionality you want when the assistant is clicked.
+    // For example, open a chat/help modal or navigate to a help page.
+    alert('Assistant Clicked!');
+}
+
+/* google_login
+function onSignIn(googleUser) {
+    // Get the user's ID token and basic profile information
+    var id_token = googleUser.getAuthResponse().id_token;
+    var profile = googleUser.getBasicProfile();
+
+    // TODO: Send the ID token to your server for verification and user authentication
+    console.log('ID Token: ' + id_token);
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+}
+*/
